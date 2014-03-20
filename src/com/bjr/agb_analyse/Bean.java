3f023ba7cd8c3c83f19a1290e37cc741089a28da
raphaelschmitz <raphael.schmitz@me.com>
2014-03-20 23:35:51 +0100

    Init

---
+++ src/com/bjr/agb_analyse/Bean.java
@@ -0,0 +1,13 @@
+package com.bjr.agb_analyse;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Bean {
+
+    final String world = "Hello World!";
+
+    public String getworld() {
+        return world;
+    }
+}
