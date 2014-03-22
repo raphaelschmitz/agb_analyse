205c2eaeb7e60b50a389e35819927295796d4073
raphaelschmitz <raphael.schmitz@me.com>
2014-03-22 11:42:43 +0100

    Update

--- src/com/bjr/agb_analyse/Bean.java
+++ src/com/bjr/agb_analyse/Bean.java
@@ -1,13 +1,21 @@
 package com.bjr.agb_analyse;
 
-import javax.faces.bean.ManagedBean;
-
-@ManagedBean
-public class Bean {
-
-    final String world = "Hello World!";
-
-    public String getworld() {
-        return world;
+	import javax.faces.bean.ManagedBean;
+	import javax.faces.bean.SessionScoped;
+	import java.io.Serializable;
+	 
+	@ManagedBean
+	@SessionScoped
+	public class Bean implements Serializable {
+	 
+		private static final long serialVersionUID = 1L;
+	 
+		private String name;
+	 
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
     }
-}
